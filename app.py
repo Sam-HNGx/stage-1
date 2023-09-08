10c5e35e22218d0bb1cbae46cfe1ea31ce61060c
OlascoWorks <113733610+OlascoWorks@users.noreply.github.com>
2023-09-08 04:38:27 +0100

    changed fields

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/api')
 def home():
-    name = request.args.get('name')
+    name = request.args.get('slack_name')
     track = request.args.get('track')
     today = datetime.date.today()
     days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -14,7 +14,7 @@ def home():
         res = {
             "slack_name": name,
             "current_day": days_of_the_week[datetime.date.weekday(today)],
-            "time": time.strftime("%H:%M:%S", time.localtime()),
+            "utc_time": time.strftime("%H:%M:%S", time.localtime()),
             "track": track,
             "github_file_url": "https://github.com/Sam-HNGx/stage-1/blob/3fc862ee6851e969802bc4c12256052dedc87fd8/app.py",
             "github_repo_url": "https://github.com/Sam-HNGx/stage-1.git",
